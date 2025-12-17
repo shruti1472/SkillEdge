d2b03e70fba921444d168960b6b0475361342d55
shruti1472 <152212826+shruti1472@users.noreply.github.com>
2025-12-17 14:31:52 +0530

    Update input placeholder text for search functionality

--- frontend/src/pages/SearchWithAi.jsx
+++ frontend/src/pages/SearchWithAi.jsx
@@ -155,7 +155,7 @@ function SearchWithAi() {
 
           <input
             type="text"
-            placeholder="Tell what you want to learn..."
+            placeholder="Search the course..."
             className="bg-transparent text-white px-4 py-3 flex-grow 
               placeholder-gray-400 focus:outline-none"
             value={input}
